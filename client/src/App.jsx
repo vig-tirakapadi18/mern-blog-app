cb671e0e4d2d331c06dd8443669d1f97c4f007b4
vig-titakapadi18 <vighneshvt2000@gmail.com>
2024-03-26 12:32:32 +0530

    added routes and navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,45 @@
+import { Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+
 const App = () => {
-    return <div>Hello from Client!</div>;
+    return (
+        <Fragment>
+            <Header />
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
+                <Route
+                    path="/about"
+                    element={<About />}
+                />
+                <Route
+                    path="/projects"
+                    element={<Projects />}
+                />
+                <Route
+                    path="/dashboard"
+                    element={<Dashboard />}
+                />
+                <Route
+                    path="/sign-in"
+                    element={<SignIn />}
+                />
+                <Route
+                    path="/sign-up"
+                    element={<SignUp />}
+                />
+            </Routes>
+        </Fragment>
+    );
 };
 
 export default App;
