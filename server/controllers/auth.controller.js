43ac37f634a1529e47ea7133a6343cb313876651
vig-titakapadi18 <vighneshvt2000@gmail.com>
2024-04-06 21:35:18 +0530

    added admin functionality

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -41,7 +41,7 @@ exports.signIn = async (req, res, next) => {
             return next(errorHandler(400, "Invalid credentials."));
         }
 
-        const token = jwt.sign({ id: validEmail._id }, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: validEmail._id, isAdmin: validEmail.isAdmin }, process.env.JWT_SECRET);
 
         const { password: pass, ...rest } = validEmail._doc;
 
@@ -58,7 +58,7 @@ exports.google = async (req, res, next) => {
         const user = await User.findOne({ email });
 
         if (user) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
             const { password, ...rest } = user._doc;
             res.status(200).cookie("token", token, {
                 httpOnly: true
@@ -73,7 +73,7 @@ exports.google = async (req, res, next) => {
                 profilePicture: googlePhotoUrl
             });
             await newUser.save();
-            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
             const { password, ...rest } = newUser._doc;
             res.status(200).cookie("token", token, { httpOnly: true }).json(rest);
         }
